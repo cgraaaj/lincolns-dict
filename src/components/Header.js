624c7f666334916cad04d0c3b5cf2ced1bdc24b1
pudge <cgrvel@gmail.com>
2021-06-14 20:12:01 +0530

    deploy run as deamon

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
     return (
       <div className="ui secondary  menu">
         <Link to="/books" className="item">
-          Books
+          Booms
         </Link>
         <Link to="/" className="item">
           Dictionary
